07867c5975ec4bb922c30c6640ceaf256c516e67
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2018-09-21 12:38:37 -0400

    got to props

---
+++ src/components/search_bar.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class SearchBar extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {term: ''}
+    }
+    
+    render () {
+        return (
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value}) }/>
+                The value of input is: {this.state.term}
+            </div>
+        );
+    }
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
+import VideoList from './components/video_list';
+
+const API_KEY = "AIzaSyBND_nr8gqFs5zMaCOtL-KiaPwOsmHD6LA";
+
 // Create a new component that will make some html.
- 
+// Take the components html and add it to the page, the DOM.
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos: videos });
+        });
+    }
+    
+    
+    
+    render () {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
